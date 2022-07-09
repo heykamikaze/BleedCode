2803ad5a83edb5db5e1b08d5a4f9dc06458a7195
Millie <Millie@MacBook-Pro-Kamilla.local>
2022-07-10 00:13:45 +0300

    binary tree sum

---
+++ Root_Equals_Sum.go
@@ -0,0 +1,13 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func checkTree(root *TreeNode) bool {
+    if root.Left.Val + root.Right.Val == root.Val{
+        return true } else {
+        return false}
+}
